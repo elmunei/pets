3522f647be7c4d758fd5977cbaa6dafc0a86bad8
Vasil Nunev <vasil.nunev@pagesuite.com>
2016-11-28 11:48:22 +0000

    first commit

--- InstagramLike/LoginViewController.swift
+++ InstagramLike/LoginViewController.swift
@@ -7,29 +7,41 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var pwField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        
+        guard emailField.text != "", pwField.text != "" else {return}
+        
+        FIRAuth.auth()?.signIn(withEmail: emailField.text!, password: pwField.text!, completion: { (user, error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            
+            if let user = user {
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "usersVC")
+                
+                self.present(vc, animated: true, completion: nil)
+            }
+        })
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- InstagramLike/SignupViewController.swift
+++ InstagramLike/SignupViewController.swift
@@ -7,29 +7,124 @@
 //
 
 import UIKit
+import Firebase
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var comPwField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    let picker = UIImagePickerController()
+    var userStorage: FIRStorageReference!
+    var ref: FIRDatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        picker.delegate = self
+        
+        let storage = FIRStorage.storage().reference(forURL: "gs://instagram-f3f20.appspot.com")
+        
+        ref = FIRDatabase.database().reference()
+        userStorage = storage.child("users")
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func selectImagePressed(_ sender: Any) {
+        
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.imageView.image = image
+            nextBtn.isHidden = false
+        }
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    @IBAction func nextPressed(_ sender: Any) {
+        
+        guard nameField.text != "", emailField.text != "", password.text != "", comPwField.text != "" else { return}
+        
+        if password.text == comPwField.text {
+            FIRAuth.auth()?.createUser(withEmail: emailField.text!, password: password.text!, completion: { (user, error) in
+                
+                
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+                
+                if let user = user {
+                    
+                    let changeRequest = FIRAuth.auth()!.currentUser!.profileChangeRequest()
+                    changeRequest.displayName = self.nameField.text!
+                    changeRequest.commitChanges(completion: nil)
+                    
+                    let imageRef = self.userStorage.child("\(user.uid).jpg")
+                    
+                    let data = UIImageJPEGRepresentation(self.imageView.image!, 0.5)
+                    
+                    let uploadTask = imageRef.put(data!, metadata: nil, completion: { (metadata, err) in
+                        if err != nil {
+                            print(err!.localizedDescription)
+                        }
+                        
+                        imageRef.downloadURL(completion: { (url, er) in
+                            if er != nil {
+                                print(er!.localizedDescription)
+                            }
+                            
+                            
+                            if let url = url {
+                                
+                                let userInfo: [String : Any] = ["uid" : user.uid,
+                                                                "full name" : self.nameField.text!,
+                                                                "urlToImage" : url.absoluteString]
+                                
+                                self.ref.child("users").child(user.uid).setValue(userInfo)
+                                
+                                
+                                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "usersVC")
+                                
+                                self.present(vc, animated: true, completion: nil)
+                                
+                            }
+                            
+                        })
+                        
+                    })
+                    
+                    uploadTask.resume()
+                    
+                }
+                
+                
+            })
+            
+            
+            
+        } else {
+            print("Password does not match")
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
 
 }
